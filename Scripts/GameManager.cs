145e6503db4c52fae4335afa0cc986ca9a8d4b78
Martin Řehořek <89700194+MartinRehorek@users.noreply.github.com>
2022-06-18 17:57:46 +0200

    -whitespaces

--- Scripts/GameManager.cs
+++ Scripts/GameManager.cs
@@ -9,46 +9,25 @@ public class GameManager : MonoBehaviour
 
 {
     public GameObject GameOverImage;
-
-
     public GameObject gameOverCanvas;
-
     public GameObject ReplaySceneCanvas;
-
-    
-
-
-    
-
     private void Start()
     {
-        
         gameOverCanvas.SetActive(false);
         GameOverImage.SetActive(false);
         ReplaySceneCanvas.SetActive(false);
-        
-        Time.timeScale = 1;
-        
+        Time.timeScale = 1;   
     }
-
     public void GameOver()
-
     {
         GameOverImage.SetActive(true);
         gameOverCanvas.SetActive(true);
         ReplaySceneCanvas.SetActive(true);
-        
         Time.timeScale = 0;
-     
     }
     public void Replay()
     {
         SceneManager.LoadScene(0);
 
     }
-    
-
-    
-   
-
 }
